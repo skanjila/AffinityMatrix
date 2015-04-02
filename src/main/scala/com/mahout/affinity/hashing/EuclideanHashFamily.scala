902b70ee022cd10a85ebe7381a9a38927c1e0a9e
Saikat Kanjilal <saikat.kanjilal@disney.com>
2015-04-01 22:28:41 -0700

    locality sensitive hashing coded up

---
+++ src/main/scala/com/mahout/affinity/hashing/EuclideanHashFamily.scala
@@ -0,0 +1,15 @@
+package com.mahout.affinity.hashing
+
+import java.util.Arrays
+//remove if not needed
+import scala.collection.JavaConversions._
+
+@SerialVersionUID(3406464542795652263L)
+class EuclidianHashFamily(private var w: Int, private val dimensions: Int) extends HashFamily {
+
+  override def createHashFunction(): HashFunction = new EuclideanHash(dimensions, w)
+
+  override def combine(hashes: Array[Int]): java.lang.Integer = Arrays.hashCode(hashes)
+
+  override def createDistanceMeasure(): DistanceMeasure = new EuclideanDistance()
+}
